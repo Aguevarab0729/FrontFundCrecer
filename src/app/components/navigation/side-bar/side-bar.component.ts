a1eed9d541d61699a344c31f51d13e938beb206d
Angélica Guevara <angelica0729@gmail.com>
2023-03-16 14:02:38 -0500

    sidebar

--- src/app/components/navigation/side-bar/side-bar.component.ts
+++ src/app/components/navigation/side-bar/side-bar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss']
 })
-export class SideBarComponent {
+export class SideBarComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.burguer()
+  }
+
+  burguer(){
+  const btn =  document.querySelector('#menu-btn');
+  const menu = document.querySelector('#sidemenu');
+  btn?.addEventListener('click', i =>{
+    menu?.classList.toggle("menu-expanded");
+    menu?.classList.toggle("menu-collapsed")
+
+    document.querySelector('body')?.classList.toggle('body-collapsed');
+
+  });
+  }
 }
+
