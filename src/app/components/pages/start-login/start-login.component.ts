cdeb98c2ae72b19b27d291f8f4a1979d587471d1
ManeDM <mane657@hotmail.com>
2023-03-28 09:35:51 -0500

    Fixing Acordeon Information

--- src/app/components/pages/start-login/start-login.component.ts
+++ src/app/components/pages/start-login/start-login.component.ts
@@ -1,6 +1,7 @@
 
 import { Component } from '@angular/core';
 import { FormBuilder, AbstractControlOptions, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-login',
